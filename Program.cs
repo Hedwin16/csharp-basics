e07a47eaa777bcc1917e4443cf0f06697fb1eac3
Hedwin <ds.hedwin@gmail.com>
2022-04-17 21:27:02 -0400

    listas y bucles

--- Program.cs
+++ Program.cs
@@ -3,3 +3,15 @@ Console.WriteLine("Hello World");
 int num=123;
 object o = num;
 Console.WriteLine("The num is "+o);
+List<int> lista = new List<int>();
+lista.Add(20);
+lista.Add(30);
+lista.Add(34);
+lista.Add(50);
+lista.Add(2120);
+
+Console.WriteLine("---Lista de Números---");
+for (int i = 0; i < lista.Count; i++)
+{
+    Console.WriteLine("Número: "+lista[i]);
+}
