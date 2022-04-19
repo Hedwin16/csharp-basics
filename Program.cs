90f1a79481d4b0df9bcffae6d620884997bd384e
Hedwin <ds.hedwin@gmail.com>
2022-04-18 20:58:06 -0400

    Variables y metodos

--- Program.cs
+++ Program.cs
@@ -1,17 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello World");
-int num=123;
-object o = num;
-Console.WriteLine("The num is "+o);
-List<int> lista = new List<int>();
-lista.Add(20);
-lista.Add(30);
-lista.Add(34);
-lista.Add(50);
-lista.Add(2120);
+// Console.WriteLine("Hello World");
+// int num=123;
+// object o = num;
+// Console.WriteLine("The num is "+o);
+// List<int> lista = new List<int>();
+// lista.Add(20);
+// lista.Add(30);
+// lista.Add(34);
+// Console.WriteLine("---Lista de Números---");
+// for (int i = 0; i < lista.Count; i++)
+// {
+//     Console.WriteLine("Número: "+lista[i]);
+// }
 
-Console.WriteLine("---Lista de Números---");
-for (int i = 0; i < lista.Count; i++)
+Console.WriteLine("Introduce tu nombre: ");
+string nombre = "Nombre";
+nombre = Console.ReadLine();
+Console.WriteLine("Introduce tu apellido: ");
+string apellido = Console.ReadLine();
+Console.WriteLine("Tu nombre Completo es: "+nombre+" "+apellido);
+Console.WriteLine("Escribe un número para sumarlo: ");
+string txtNum1 = Console.ReadLine();
+Console.WriteLine("Escribe otro número para sumarlo: ");
+string txtNum2 = Console.ReadLine();
+
+int num1, num2;
+num1 = int.Parse(txtNum1);
+num2 = int.Parse(txtNum2);
+int resultado = sumar(num1,num2);
+
+Console.WriteLine("El resultado de la suma es: "+resultado);
+Console.WriteLine("Tu nombre en mayúscula es: "+nombre.ToUpper());
+
+
+int sumar(int num1, int num2)
 {
-    Console.WriteLine("Número: "+lista[i]);
+    return num1+num2;
 }
