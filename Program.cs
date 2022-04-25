7757b2fd5d91a748f2d3c489b20e5deebd4c8184
Hedwin Oficina <ds.hedwin+oficina@gmail.com>
2022-04-25 11:07:59 -0800

    first init linq

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int[] numeros = new[] {1,2,3,4,5,6,7,8,9};
+
+System.Console.WriteLine("Números impares: ");
+IEnumerable<int> impares = from numero in numeros where numero %2 != 0 select numero;
+System.Console.WriteLine("Cantidad de Números impares: "+impares.Count());
+foreach(int impar in impares){
+    System.Console.WriteLine("Impar: "+impar);
+}
