b6a62bdc8ebfd98b8d4a33e9949734e6224eaff2
Hedwin Oficina <ds.hedwin+oficina@gmail.com>
2022-04-25 16:14:12 -0800

    IsAlive & Join

--- Program.cs
+++ Program.cs
@@ -1,51 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-System.Console.WriteLine("Threads - Hilos");
+﻿System.Console.WriteLine("Main ha comenzado");
+Thread hilo1 = new Thread(FunctionThread1);
+Thread hilo2 = new Thread(FunctionThread2);
+hilo1.Start();
+hilo2.Start();
 
-// System.Console.WriteLine("Hola Mundo 1");
-// Thread.Sleep(1000);
-// System.Console.WriteLine("Hola Mundo 2");
-// Thread.Sleep(1000);
-// System.Console.WriteLine("Hola Mundo 3");
-// Thread.Sleep(1000);
-// System.Console.WriteLine("Hola Mundo 4");
-// Thread.Sleep(1000);
-// System.Console.WriteLine("Hola Mundo 5");
-
-// new Thread(()=>
-// {
-//     Thread.Sleep(1000);
-//     Console.WriteLine("Subproceso 1");
-// }).Start();
-
-// new Thread(()=>
-// {
-//     Thread.Sleep(1000);
-//     Console.WriteLine("Subproceso 2");
-// }).Start();
-
-// new Thread(()=>
-// {
-//     Thread.Sleep(1000);
-//     Console.WriteLine("Subproceso 3");
-// }).Start();
-
-// new Thread(()=>
-// {
-//     Thread.Sleep(1000);
-//     Console.WriteLine("Subproceso 4");
-// }).Start();
-
-
-Enumerable.Range(0, 1000).ToList().ForEach(f =>
+// hilo1.Join();
+// System.Console.WriteLine("Función1 está Listo");
+if (hilo1.Join(1000))
 {
-    ThreadPool.QueueUserWorkItem((o) =>
+    System.Console.WriteLine("Funcion 1 Terminada");
+}
+else
+{
+
+    System.Console.WriteLine("Funcion 1 no terminó en 1 segundo");
+}
+
+hilo2.Join();
+System.Console.WriteLine("Función2 está Listo");
+for (int i = 0; i < 10; i++)
+{
+    if (hilo1.IsAlive)
     {
-        System.Console.WriteLine($"Número de Subproceso: {Thread.CurrentThread.ManagedThreadId} ha iniciado");
-        Thread.Sleep(5000);
-        System.Console.WriteLine($"Número de Subproceso: {Thread.CurrentThread.ManagedThreadId} ha terminado");
+        System.Console.WriteLine("Hilo 1 sigue ejecutandose");
+        Thread.Sleep(1000);
+    }
+    else
+    {
+        System.Console.WriteLine("Hilo 1 terminó la ejecución");
+    }
 
-    });
-});
 
+}
 
+System.Console.WriteLine("Main ha finalizado");
+
+static void FunctionThread1()
+{
+    System.Console.WriteLine("Inició FuncionThread1");
+    Thread.Sleep(3000);
+    System.Console.WriteLine("Funcion1 regresa al main");
+}
+
+static void FunctionThread2()
+{
+    System.Console.WriteLine("Inició FuncionThread1");
+}
